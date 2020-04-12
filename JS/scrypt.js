7908df9f337823e0401081917e6f77c8a0b5842e
Emily Piazza Lopes <37214001+rukilopes@users.noreply.github.com>
2020-04-12 13:46:11 -0300

    definindo movimento da cobrinha

--- JS/scrypt.js
+++ JS/scrypt.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -19,5 +20,28 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right")snakeX += box;
+    if(direction = "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop(); //retirar o último elemento do array
+
+    let newhead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newhead);
+}
+
+let jogo = setInterval(iniciarJogo,100);//100 milisegundos para reiniciar o jogo
+
+
